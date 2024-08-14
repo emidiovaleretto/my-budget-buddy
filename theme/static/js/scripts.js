11663cca497ba021bc4b98affd7f70ad73e06947
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-14 20:43:29 +0100

    chore: add js scripts

---
+++ theme/static/js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    let currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+
+})
