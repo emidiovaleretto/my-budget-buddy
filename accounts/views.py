b2c3cf7caae63a9c4d27d4147da62a2235b7eb7d
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-16 20:36:26 +0000

    fix: profiles app reference name

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'profiles/home.html')
+    return render(request, 'accounts/home.html')
 
 
 def manage(request):
-    return render(request, 'profiles/manage.html')
+    return render(request, 'accounts/manage.html')
