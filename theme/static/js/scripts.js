669efc153ad8d63bf556761a05dbff26ce20f8e7
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-17 14:58:29 +0100

    chore: implement a function to toggle the input field when user chooses 'others'

--- theme/static/js/scripts.js
+++ theme/static/js/scripts.js
@@ -3,4 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentYear = new Date().getFullYear();
     document.getElementById('currentYear').textContent = currentYear;
 
+    console.log('scripts.js loaded');
+
+    let selectBox = document.getElementById("bank");
+    let otherBankInput = document.getElementById("otherBank");
+
+    let expenseCategory = document.getElementById("expenseCategory");
+    let otherCategory = document.getElementById("otherCategory");
+    
+    selectBox.addEventListener("change", function() {
+        console.log(selectBox.value);
+        let otherBankDiv = document.getElementById("otherBankDiv");
+        if (selectBox.value === "other") {
+            otherBankDiv.classList.remove("hidden");
+            otherBankInput.focus();
+        } else {
+            otherBankDiv.classList.add("hidden");
+        }
+    });
+
+    expenseCategory.addEventListener("change", function() {
+        console.log(expenseCategory.value);
+        if (expenseCategory.value === "other") {
+            otherCategoryDiv.classList.remove("hidden");
+            otherCategory.focus();
+        } else {
+            otherCategoryDiv.classList.add("hidden");
+        }
+    });
 })
