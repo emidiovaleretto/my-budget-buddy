aff6d0f55e0e9e4264f9652cea05f596eb15633c
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-19 10:51:00 +0100

    chore: remove 'balance' attribute from Categories model

--- accounts/models/Categories.py
+++ accounts/models/Categories.py
@@ -5,7 +5,6 @@ from accounts.models import *
 class Category(models.Model):
     name = models.CharField(max_length=50)
     is_essential = models.BooleanField(default=False)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
