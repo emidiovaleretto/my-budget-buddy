206c91bcdd1dc1c1273565cb65aea2ecd9c24b96
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-19 19:33:36 +0000

    chore: implement new_balance views

--- statements/views.py
+++ statements/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from accounts.models.Accounts import Account
+from accounts.models.Categories import Category
+
+from datetime import date
+
 
 def new_balance(request):
-    ...
+    if request.method == "GET":
+        accounts = Account.objects.all()
+        categories = Category.objects.all()
+        current_date = date.today()
+
+        print(current_date)
+
+        context = {
+            'accounts': accounts,
+            'categories': categories,
+            'current_date': current_date,
+        }
+
+        return render(request, 'statements/add_entries.html', context=context)
