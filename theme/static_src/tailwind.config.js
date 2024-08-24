702b66f8ada81057f9ff27e3934819df1879a229
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-24 15:41:53 +0100

    chore: add complementary color

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -48,6 +48,7 @@ module.exports = {
                 'light-gray': '#2B2B2B',
                 'accent': '#69D2CD',
                 'accent-hover': '#5AC7C1',
+                'complementary': '#69a2d1', 
             },
             fontFamily: {
                 'sans': ['"Roboto", sans-serif'],
