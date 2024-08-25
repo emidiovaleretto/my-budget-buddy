b6df6915a7d732fd81d5d7f731749f34a37c5bc3
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-25 12:11:07 +0100

    fix: ZeroDivisionError

--- accounts/models/Categories.py
+++ accounts/models/Categories.py
@@ -21,18 +21,18 @@ class Category(models.Model):
             return mark_safe('<span class="badge-success">Yes</span>')
         return mark_safe('<span class="badge-danger">No</span>')
 
-    def get_total_expense(self):
+    def get_total_expenses(self):
         from incomes.models.Incomes import Income
 
-        incomes = Income.objects.filter(category_id=self.id).filter(
+        incomes = Income.objects.filter(category__id=self.id).filter(
             date__month=datetime.now()
             .month).exclude(
-                type_of_entry__in=['IN', 'FX']
-            ).aggregate(Sum('amount'))
+                type_of_entry__icontains=['in', 'fix']
+        ).aggregate(Sum('amount'))
         return incomes['amount__sum'] if incomes['amount__sum'] else 0
 
-    def get_percentage(self):
+    def get_percentage_per_category(self):
         try:
-            return (self.get_total_expense() * 100) / self.budget
-        except ZeroDivisionError:
+            return (self.get_total_expenses() * 100) / self.budget
+        except Exception as e:
             return 0
