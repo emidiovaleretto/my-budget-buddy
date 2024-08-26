6499a239b925e9f12eed2b11e037419477a97999
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-26 18:27:00 +0000

    chore: create Payable and Paid classes for bills

---
+++ bills/models/Bills.py
@@ -0,0 +1,19 @@
+from django.db import models
+from banking.models.Categories import Category
+
+
+class Payable(model.Models):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=3)
+    due_date = models.DateField(auto_now=False, auto_now_add=False)
+    logo = models.ImageField(upload_to="logos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Paid(model.Models):
+    bill = models.ForeignKey(Payable, on_delete=models.CASCADE)
+    paid_date = models.DateField(auto_now=False, auto_now_add=False)
