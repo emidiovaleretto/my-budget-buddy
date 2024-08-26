36b94d968a0e817ee0e8f52373d305bedba8f0c9
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-26 17:41:33 +0000

    chore: create the function to display the form to set up a new bill

--- bills/views.py
+++ bills/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from banking.models.Categories import Category
 
-# Create your views here.
+
+def set_up(request):
+    categories = Category.objects.all()
+    context = {
+        "categories": categories
+    }
+
+    return render(request, "bills/set_up.html", context=context)
