eb76c0f61450fb42a4e0010887ed1f5b6eaa418b
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-30 19:26:54 +0000

    chore: re-write the calculate_total function

--- banking/utils.py
+++ banking/utils.py
@@ -1,6 +1,4 @@
-def calculate_total(obj, field):
-    total = 0
-    for i in obj:
-        total += getattr(i, field)
+from django.db.models import Sum
 
-    return total
+def calculate_total(queryset, field_name):
+    return queryset.aggregate(total=Sum(field_name))['total'] or 0
